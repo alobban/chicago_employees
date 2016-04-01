973905fdeca559af0c8d4f970b1af9edd105b58e
Andrew Lobban <andrewglobban@gmail.com>
2016-03-31 19:06:18 -0500

    Add Complete Code

--- lib/chicago_employees.rb
+++ lib/chicago_employees.rb
@@ -1,5 +1,36 @@
 require "chicago_employees/version"
+require "unirest"
 
 module ChicagoEmployees
-  # Your code goes here...
+  class Employee
+  	attr_reader :title, :department, :name, :salary, :first_name, :last_name
+
+  	def initialize(input_options)
+  		@title = input_options["job_titles"]
+  		@department = input_options["department"]
+  		@name = input_options["name"]
+  		@salary = input_options["employee_annual_salary"]
+  		@first_name = @name.split(", ")[1]
+  		@last_name = @name.split(", ")[0]
+  	end
+
+  	def self.all
+  		ruby_data = []
+  		bulk_data = Unirest.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
+  			.body
+  			.map { |employee| Employee.new(employee) }
+  		end
+  	end
+
+  	def self.find(parameter_option)
+  		ruby_data = []
+  		bulk_data = Unirest.get(
+  			"https://data.cityofchicago.org/resource/xzkq-xp2w.json?department=#{parameter_option}"
+  		).body
+  		bulk_data.each do |employee|
+  			ruby_data << Employee.new(employee)
+  		end
+  		ruby_data
+  	end
+  end
 end
